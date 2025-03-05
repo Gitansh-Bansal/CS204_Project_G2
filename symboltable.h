5fde00ec22091c0dfe86d701c5e6c7e7c2bfa17d
mohak-7 <141384034+mohak-7@users.noreply.github.com>
2025-03-05 18:20:37 +0530

    Create symboltable.h

---
+++ symboltable.h
@@ -0,0 +1,43 @@
+#ifndef SYMBOLTABLE_H
+#define SYMBOLTABLE_H
+
+#include <string>
+#include <unordered_map>
+#include <cstdint>
+
+const uint32_t TEXT_SEGMENT_START = 0x00000000;
+const uint32_t DATA_SEGMENT_START = 0x10000000;
+const uint32_t HEAP_SEGMENT_START = 0x10008000;
+const uint32_t STACK_SEGMENT_START = 0x7FFFFDC;
+
+enum MemorySegment {
+    TEXT,
+    DATA,
+    HEAP,
+    STACK
+};
+
+class SymbolTable {
+private:
+    unordered_map<string, uint32_t> symbols;
+    
+    uint32_t textCursor;
+    uint32_t dataCursor;
+    
+    MemorySegment currentSegment;
+
+public:
+    SymbolTable();
+    
+    void addSymbol(const string& name, uint32_t address);
+    uint32_t getSymbolAddress(const string& name) const;
+    bool hasSymbol(const string& name) const;
+    void setCurrentSegment(MemorySegment segment);
+    MemorySegment getCurrentSegment() const;
+    uint32_t getCurrentAddress() const;
+    void incrementAddress(uint32_t increment);
+    void resetCursors();
+    void printSymbolTable() const;
+};
+
+#endif
