fd969703b6befd2176731d7d06b22d1188f5484e
mohak-7 <141384034+mohak-7@users.noreply.github.com>
2025-03-05 12:11:31 +0530

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,38 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum InstructionType {
+    R_TYPE,
+    I_TYPE,
+    S_TYPE,
+    SB_TYPE,
+    U_TYPE,
+    UJ_TYPE,
+    DIRECTIVE,
+    LABEL,
+    UNKNOWN
+};
+
+struct Instruction {
+    string label;
+    string opcode; 
+    vector<std::string> operands;
+    string comment;
+    InstructionType type;      
+    int lineNumber;  
+    bool hasLabel; 
+};
+
+bool isDirective(const string& token);
+InstructionType getInstructionType(const string& opcode);
+Instruction parseLine(const string& line, int lineNumber);
+vector<Instruction> parseFile(const string& filename);
+string trimString(const string& str);
+vector<string> splitString(const string& str, char delimiter);
+string removeComments(const string& line);
+
+#endif 
