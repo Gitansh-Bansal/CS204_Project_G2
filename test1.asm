c2e7493e0add9c7be28500a355a028bc1b2ad822
mohak-7 <141384034+mohak-7@users.noreply.github.com>
2025-03-08 11:21:25 +0530

    Add input output sample

---
+++ test1.asm
@@ -0,0 +1,47 @@
+.data
+array : .byte -12 40 2 13 73 29 12 88 15 7
+
+.text 
+lui x5, 0x10000     # x5 has the starting address
+lui x6, 0x10000     # (to find) end address in x6
+lb x7, 0, x6        # x7 contains whatever at x6
+addi x8, x0, 0      # x8 will contain the length of the array
+
+findlen : beq x7, x0, lenfound
+addi x8, x8, 1
+addi x6, x6, 1
+lb x7, 0, x6
+jal x0, findlen
+
+lenfound:   addi x8, x8, -1
+# now x8 contains the length of the array
+
+addi x6, x0, 0
+addi x7, x0, 0
+
+add x30, x0, x8     # outer loop counter
+add x31, x0, x8     # inner loop counter
+
+
+outerloop : beq x30, x0, exit 
+add x31, x0, x8
+lui x6, 0x10000
+innerloop : beq x31, x0, outofinner
+lb x10, 0, x6
+lb x11, 1, x6
+blt x11, x10, exchange10x11
+retn : addi x6, x6, 1
+addi x31, x31, -1
+jal x0, innerloop
+
+outofinner : addi x30, x0, -1
+jal x0, outerloop
+
+exchange10x11 : addi x28, x10, 0
+addi x29, x11, 0
+sb x28, 1, x6
+sb x29, 0, x6
+jal x0, retn
+
+exit:
+
