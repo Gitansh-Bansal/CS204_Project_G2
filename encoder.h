1ac4fd1c32a54c9a2317bc22625c9df2d078e9a9
Gitansh Bansal <142196514+Gitansh-Bansal@users.noreply.github.com>
2025-03-09 13:07:23 +0530

    added comments to encoder.h

--- encoder.h
+++ encoder.h
@@ -1,16 +1,16 @@
 #ifndef ENCODER_H
 #define ENCODER_H
-
 #include <string>
 #include <cstdint>
 #include <vector>
 #include "parser.h"
 #include "symboltable.h" 
-
 using namespace std;
 
+// function to encode an instruction
 uint32_t encodeInstruction(const Instruction& instr, uint32_t addr, const SymbolTable& symbolTable);
 
+// functions for encoding different types of instructions
 uint32_t encodeR(const Instruction& instr);
 uint32_t encodeI(const Instruction& instr, uint32_t addr, const SymbolTable& symbolTable);
 uint32_t encodeS(const Instruction& instr, uint32_t addr, const SymbolTable& symbolTable);
@@ -18,10 +18,14 @@ uint32_t encodeSB(const Instruction& instr, uint32_t addr, const SymbolTable& sy
 uint32_t encodeU(const Instruction& instr, uint32_t addr, const SymbolTable& symbolTable);
 uint32_t encodeUJ(const Instruction& instr, uint32_t addr, const SymbolTable& symbolTable);
 
+// functions for encoding directives
 vector<uint8_t> encodeDirective(const Instruction& instr);
+
+// functions for gnerating the output line for an instruction
 string generateEncodingComment(const Instruction& instr, uint32_t machineCode);
 string formatOutputLine(uint32_t addr, uint32_t machineCode, const Instruction& instr, const string& encodingComment);
 
+// helper functions for encoding
 int32_t parseRegister(const string& reg);
 int32_t parseImmediate(const string& imm);
 string intToHex(uint32_t value);
