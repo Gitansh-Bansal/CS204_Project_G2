25f8f4143fcd0027eb3f21003021dc573b42edd8
Gitansh Bansal <142196514+Gitansh-Bansal@users.noreply.github.com>
2025-03-21 11:44:24 +0530

    memoryAccess function written

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -50,6 +50,78 @@ Simulator::DecodedInstruction Simulator::decode(uint32_t instruction)
 
 void Simulator::execute(DecodedInstruction& decodedInst) 
 
-void Simulator::memoryAccess(DecodedInstruction& decodedInst) 
+void Simulator::memoryAccess(DecodedInstruction& decodedInst) {
+    cout << "\nMEMORY ACCESS:" << endl;
+    
+    uint32_t opcode = decodedInst.opcode;
+    uint32_t funct3 = decodedInst.funct3;
+    
+    uint32_t address = regState.getTemp("MAR");
+    
+    switch (opcode) {
+        case 0x03: {
+            
+            switch (funct3) {
+                case 0x0: 
+                    regState.setTemp("MDR",static_cast<int32_t>(static_cast<int8_t>(memory.readByte(address))));
+                    regState.setTemp("RY",static_cast<int32_t>(static_cast<int8_t>(memory.readByte(address))));
+                    break;
+                    
+                case 0x1: 
+                    regState.setTemp("MDR",static_cast<int32_t>(static_cast<int16_t>(memory.readHalf(address))));
+                    regState.setTemp("RY",static_cast<int32_t>(static_cast<int16_t>(memory.readHalf(address))));
+                    break;
+                    
+                case 0x2: 
+                    regState.setTemp("MDR",static_cast<int32_t>(memory.readWord(address)));
+                    regState.setTemp("RY",static_cast<int32_t>(memory.readWord(address)));
+                    break;
+
+                case 0x3:
+                    cerr << "Error: 64-bit load not supported" << endl;
+                    break;
+
+                default:
+                    cout << "Unknown load operation (funct3: 0x" << hex << funct3 << dec << ")" << endl;
+                    break;
+            }
+            break;
+        }
+        
+        case 0x23: {
+            uint32_t data = regState.getTemp("RB");
+            
+            switch (funct3) {
+                case 0x0: 
+                    memory.writeByte(address, data & 0xFF);
+                    break;
+                    
+                case 0x1: 
+                    memory.writeHalf(address, data & 0xFFFF);
+                    break;
+                    
+                case 0x2: 
+                    memory.writeWord(address, data);
+                    break;
+
+                case 0x3:
+                    cerr << "Error: 64-bit store not supported" << endl;
+                    break;
+                    
+                default:
+                    cout << "Unknown store operation with funct3: 0x" << hex << funct3 << dec << endl;
+                    break;
+            }
+            break;
+        }
+        
+        default:
+            regState.tempReg["RY"] = regState.tempReg["RZ"];
+            break;
+    }
+    
+    pc = regState.tempReg["PC"];
+}
+
 
 void Simulator::writeBack(DecodedInstruction& decodedInst)
