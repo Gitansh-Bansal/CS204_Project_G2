332236fbf6dc96053ddd514a346851548abae9db
mohak-7 <141384034+mohak-7@users.noreply.github.com>
2025-03-21 22:18:27 +0530

    update simulator.cpp

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -83,8 +83,6 @@ Simulator::DecodedInstruction Simulator::decode(uint32_t instruction) {
             decodedInst.rs1    = (instruction >> 15) & 0x1F;
             decodedInst.rs2    = (instruction >> 20) & 0x1F;
             decodedInst.funct7 = (instruction >> 25) & 0x7F;
-            regState.setTemp("RA", decodedInst.rs1);
-            regState.setTemp("RB", decodedInst.rs2);
             break;
         case(0x13): // i type
             decodedInst.rd     = (instruction >> 7)  & 0x1F;
@@ -93,8 +91,6 @@ Simulator::DecodedInstruction Simulator::decode(uint32_t instruction) {
             decodedInst.imm = (instruction >> 20) & 0xFFF;
             if (decodedInst.imm & 0x800)
                 decodedInst.imm |= 0xFFFFF000;
-            regState.setTemp("RA", decodedInst.rs1);
-            regState.setTemp("IMM", decodedInst.imm);
             break;
         case(0x03):  // i type load instructions
             decodedInst.rd     = (instruction >> 7)  & 0x1F;
@@ -103,8 +99,6 @@ Simulator::DecodedInstruction Simulator::decode(uint32_t instruction) {
             decodedInst.imm = (instruction >> 20) & 0xFFF;
             if (decodedInst.imm & 0x800)
                 decodedInst.imm |= 0xFFFFF000;
-            regState.setTemp("RA", decodedInst.rs1);
-            regState.setTemp("IMM", decodedInst.imm);
             break;
         case(0x23): // s type
             decodedInst.imm = (instruction >> 7) & 0x1F;
@@ -114,9 +108,6 @@ Simulator::DecodedInstruction Simulator::decode(uint32_t instruction) {
             decodedInst.imm |= ((instruction >> 25) & 0x7F) << 5;
             if (decodedInst.imm & 0x800)
                 decodedInst.imm |= 0xFFFFF000;            
-            regState.setTemp("RA", decodedInst.rs1);
-            regState.setTemp("RB", decodedInst.rs2);
-            regState.setTemp("IMM", decodedInst.imm);
             break;
         case(0x63): // sb type
             decodedInst.funct3 = (instruction >> 12) & 0x07;
@@ -128,9 +119,6 @@ Simulator::DecodedInstruction Simulator::decode(uint32_t instruction) {
                              ((instruction >> 7) & 0x1) << 11;
             if (decodedInst.imm & 0x1000)
                 decodedInst.imm |= 0xFFFFE000;
-            regState.setTemp("RA", decodedInst.rs1);
-            regState.setTemp("RB", decodedInst.rs2);
-            regState.setTemp("IMM", decodedInst.imm);
             break;
         case(0x6F): // uj type
             decodedInst.rd     = (instruction >> 7) & 0x1F;
@@ -140,7 +128,6 @@ Simulator::DecodedInstruction Simulator::decode(uint32_t instruction) {
             decodedInst.imm   |= ((instruction >> 31) & 0x1) << 20; 
             if (decodedInst.imm & 0x100000)
                 decodedInst.imm |= 0xFFF00000;
-            regState.setTemp("IMM", decodedInst.imm);
             break;
         case(0x67):  // jalr
             decodedInst.rd     = (instruction >> 7) & 0x1F;
@@ -149,23 +136,22 @@ Simulator::DecodedInstruction Simulator::decode(uint32_t instruction) {
             decodedInst.imm    = (instruction >> 20) & 0xFFF;
             if (decodedInst.imm & 0x800)
                 decodedInst.imm |= 0xFFFFF000;
-            regState.setTemp("RA", decodedInst.rs1);
-            regState.setTemp("IMM", decodedInst.imm);
             break;
         case(0x37):  // lui
             decodedInst.rd     = (instruction >> 7) & 0x1F;
             decodedInst.imm    = (instruction & 0xFFFFF000);
-            regState.setTemp("IMM", decodedInst.imm);
             break;
         case(0x17):  // auipc
             decodedInst.rd     = (instruction >> 7) & 0x1F;
             decodedInst.imm    = (instruction & 0xFFFFF000);
-            regState.setTemp("IMM", decodedInst.imm);
             break;
         default:     // Unknown instruction
             cout << "Unknown instruction with opcode 0x" << hex << decodedInst.opcode << dec << endl;
             break;
     }
+    regState.setTemp("RA", getGen(decodedInst.rs1));
+    regState.setTemp("RB", getGen(decodedInst.rs2));
+    regState.setTemp("IMM", decodedInst.imm);
 
     return decodedInst;
 }
